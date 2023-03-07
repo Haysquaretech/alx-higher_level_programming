3a62af3de03ce09ff21c16e231d9a4e13cf41844
Ajibade Taofeek <haysquare93@gmail.com>
2023-03-07 04:26:29 -0800

    Updated on Python if_else, loops and functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-def to_uper(character):
+def to_upper(character):
     if ord(character) >= 97 and ord(character) <= 122:
         return (ord(character) - 32)
     else:
         return ord(character)
-    ::w
+    :: w
     def uppercase(string):
+
         string_new = ""
 
         for character in string:
